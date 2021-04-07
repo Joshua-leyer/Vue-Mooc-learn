893897d169fa1773b42af499928a74daa2afad77
joshua xin <joshuaxin@163.com>
2021-04-07 12:56:51 +0800

    change

--- one-project/src/main.js
+++ one-project/src/main.js
@@ -4,12 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/style/reset.css'
-import './assets/style/border.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+require('swiper/dist/css/swiper.css')
+import 'style/reset.css'
+import 'style/border.css'
+import 'style/iconfont.css'
 
 Vue.config.productionTip = false
 // fastclick 的使用
 fastClick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper)
+
+
+
 
 /* eslint-disable no-new */
 new Vue({
---
+++ one-project/src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="iconfont back-icon">&#xe677;</span>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe615;</span>
+            请输入内容或地名
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe60a;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+//使用变量
+    @import '~style/varibles.styl'
+    .header
+        display: flex 
+        line-height: .86rem
+        background: $bgColor
+        color: #fff
+        .header-left
+            width: .64rem
+            float: left
+            .back-icon
+                // text-align: center 无效...
+                margin-left: .15rem
+                font-size: .4rem
+        .header-input 
+            flex: 1
+            height: .64rem
+            line-height .64rem
+            margin-top: .12rem
+            margin-left: .2rem
+            padding-left: .2rem
+            background: #fff
+            border-radius: .1rem
+            color: #ccc
+        .header-right
+            width 1.24rem
+            float: right
+            text-align: center
+            .arrow-icon
+                font-size: .24rem
+</style>
